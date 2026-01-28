ff57a6a9cac0258b8deb868a5a39d69cab2e5e69
John Flynn <johndangerflynn@gmail.com>
2026-01-27 22:34:24 -0500

    Initial Commit

---
+++ dot_config/mango/executable_autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set +e
+
+# obs
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1 &
+
+# dms shell
+dms run >/dev/null 2>&1 &
+
+# launch kdeconnect
+if [ -f /usr/bin/kdeconnect-indicator ]; then
+   kdeconnectd >/dev/null 2>&1 &
+   kdeconnect-indicator >/dev/null 2>&1 &
+fi
+
+#start apps
+flatpak run io.github.justinrdonnelly.bouncer >/dev/null 2>&1 &
+flatpak run com.github.zocker_160.SyncThingy >/dev/null 2>&1 &
